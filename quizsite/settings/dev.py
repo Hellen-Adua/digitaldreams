62a4e3db49759589466be7aae09aa6efb0e7aa91
Hellen-Adua <hellenaduda@gmail.com>
2025-08-27 22:31:00 +0300

    Added tailwind css popiulated database with sample questions

---
+++ quizsite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-wnvxid(i=o1fske2-5!i^wy+0hg+53)r2dt-g^(12#scm6o2*2"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
